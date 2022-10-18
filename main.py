52ee95e9f45c9d2203cc564cd7e88a1d4a6ed4aa
rakesh1268248 <114190126+rakesh1268248@users.noreply.github.com>
2022-10-18 11:49:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # from nltk.stem import WordNetLemmatizer
 # lemmatizer = WordNetLemmatizer()
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 
@@ -61,8 +62,13 @@ def st_ui(clean_text):
 #   sentences = nltk.sent_tokenize(text)
 #   documents_clean=preprocessing(sentences)  
   wordcloud = WordCloud(width = 800, height =600,background_color ='white',min_font_size = 5,max_words=500).generate(clean_text)
-  # plot the WordCloud image                      
-  return wordcloud
+  # plot the WordCloud image
+  plt.figure(figsize = (15,10), facecolor = None)
+  plt.imshow(wordcloud,interpolation="bilinear")
+  plt.axis("off")
+  plt.tight_layout(pad = 0)
+  plt.show()
+  st.pyplot(fig=plt)
 
     
  
